0b3396b84408ecc0245489004a760aa60ff38608
Pastaninja <fadhlisyukriayie@gmail.com>
2021-07-02 19:37:12 +0800

    changed default database

--- kuizonline/sambungan.php
+++ kuizonline/sambungan.php
@@ -2,7 +2,7 @@
 	$host = 'localhost';
 	$user = 'root';
 	$password = '';
-	$database = 'kuizonline2';
+	$database = 'kuizonline';
 	
 	$sambungan = mysqli_connect($host, $user, $password, $database) or die('Sambungan gagal');
 	//echo "Sambungan berjaya";
